768a01b5387dff04522aab6893d63e2641b25ce2
OlgaFomin <45686834+OlgaFomin@users.noreply.github.com>
2021-04-29 23:45:43 +0300

    finished sec7

--- main.m
+++ main.m
@@ -17,6 +17,20 @@ vertex_area = compute_vertex_area(vertices, triangles);
 visualize_fun(vertices, triangles, traingles_area, "face")
 visualize_fun(vertices, triangles, vertex_area, "vertex")
 
+%7
+[genus, boundary_edges] = calc_genus(obj.adjacency_matrix, triangles, vertices);
 
+% Analysis - 1
+off_filepath = 'hw2_data/sphere_s0.off';
+[vertices, triangles] = read_off(off_filepath);
+plot_obj(vertices, triangles);
+
+off_filepath = 'hw2_data/sphere_s2.off';
+[vertices, triangles] = read_off(off_filepath);
+plot_obj(vertices, triangles);
+
+off_filepath = 'hw2_data/sphere_s4.off';
+[vertices, triangles] = read_off(off_filepath);
+plot_obj(vertices, triangles);
 
 
